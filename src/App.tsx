f917dbd276568da26c21716bc16ecb5a2b426b68
Jared Savage <jared.savage@dialogue.co>
2020-03-04 16:07:47 -0500

    chore: setup react-simple-chatbot

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import ChatBot from 'react-simple-chatbot';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatBot
+      steps={[
+        {
+          id: 'hello-world',
+          message: 'Hello World!',
+          end: true,
+        },
+      ]}
+    />
   );
 }
 
