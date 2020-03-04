053ff16faa7c29ab336d8fdf781205ffe2433ea3
Jared Savage <jared.savage@dialogue.co>
2020-03-04 17:20:18 -0500

    chore: tweak ci config to use commands

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
 set -e
-
-if [ $CIRCLE_BRANCH == master ]; then
-  export TARGET_PATH=$1
-else
-  export TARGET_PATH=$1/branch/$CIRCLE_BRANCH
-fi
-
-echo aws s3 sync ./build s3://$TARGET_PATH --acl public-read
