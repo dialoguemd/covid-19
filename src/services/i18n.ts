e0cfc47270ed8427b9e682254436efa4dd050560
jaredsavage <jared.savage@dialogue.co>
2020-03-06 16:15:58 -0500

    feat: add i18n support (#36)

---
+++ src/services/i18n.ts
@@ -0,0 +1,29 @@
+import i18n from 'i18next'
+import i18nLanguageDetector from 'i18next-browser-languagedetector'
+
+import packageJson from '../../package.json'
+
+const resources = {
+  en: {
+    steps: require('i18n/steps.en.json')
+  },
+  fr: {
+    steps: require('i18n/steps.fr.json')
+  }
+}
+
+const fallbackLng =
+  process.env.NODE_ENV !== 'production' && packageJson.supportedLanguages
+
+const initOptions = {
+  ns: ['steps'],
+  fallbackLng,
+  initImmediate: true,
+  nonExplicitWhitelist: true,
+  resources,
+  appendNamespaceToMissingKey: true
+}
+
+i18n.use(i18nLanguageDetector).init(initOptions)
+
+export default i18n
