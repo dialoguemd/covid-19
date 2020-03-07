7d46e62e5739f09eef4b4f844770a8e6fe616294
Vanessa Jones <vanessa.jones2@mail.mcgill.ca>
2020-03-06 19:21:34 -0500

    chore: localized the flow to EN and FR (#48)

---
+++ src/i18n/translation.en.js
@@ -0,0 +1,14 @@
+export default {
+  resultsPage: {
+    headerTitle: 'Your Personalized COVID-19 Guide',
+    headerButton: 'Take the quiz again',
+    noResultsMessage: 'No results provided in URL.',
+    backToQuestionnaire: 'back to questionnaire?'
+  },
+  welcomePage: {
+    title: 'COVID-19: What you need to know',
+    description:
+      'Get accurate personalized information from trusted Canadian medical sources regarding COVID-19.',
+    button: 'Get Started'
+  }
+}
