7c39cc345b55c17939c55f54c7abd3adf51bfc76
Alexis Smirnov <alexis@smirnov.ca>
2020-03-07 19:27:34 -0500

    i18n

--- src/i18n/translation.fr.js
+++ src/i18n/translation.fr.js
@@ -3,12 +3,30 @@ export default {
     headerTitle: 'COVID-19: Ce que vous devez savoir',
     noResultsMessage: 'Aucun résultat trouvé pour cet URL',
     changeAudience: 'modifier',
-    audiencePrefix: "Les informations ci-dessous s'appliquent à"
+    audiencePrefix: "Les informations ci-dessous s'appliquent à "
   },
   welcomePage: {
     title: 'COVID-19: Ce que vous devez savoir',
     description:
       'Obtenez des informations fiables et personnalisées auprès de sources médicales canadiennes crédibles concernant le COVID-19.',
     button: 'Démarrer'
+  },
+  classes: {
+    common: 'tous les Canadiens',
+    'ca-qc': 'résidents du Québec',
+    'ca-ab': 'résidents de l\'Alberta',
+    'ca-on': 'résidents de l\'Ontario',
+    'ca-ns': 'résidents de la Nouvelle-Écosse',
+    'ca-nb': 'résidents du Nouveau-Brunswick',
+    'ca-mb': 'résidents du Manitoba',
+    'ca-bc': 'résidents de la Colombie-Britannique',
+    'ca-sk': 'résidents de la Saskatchewan',
+    'ca-pe': 'résidents de l\'Île-du-Prince-Édouard',
+    'ca-yt': 'résidents du Yukon',
+    'ca-nt': 'résidents des Territoires du Nord',
+    'ca-nl': 'résidents de Terre-Neuve-et-Labrador',
+    'ca-nu': 'Les habitants de Nuvavut',
+    'elevated-medical-risk': 'les personnes présentant un risque médical élevé',
+    'elevated-covid-risk': 'les personnes présentant un risque élevé d\'infection'
   }
 }
