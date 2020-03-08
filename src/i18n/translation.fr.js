9de59ff12bd4ce78b48b778b2008a6396829fcf9
Alexis Smirnov <alexis@smirnov.ca>
2020-03-07 19:28:20 -0500

    lint

--- src/i18n/translation.fr.js
+++ src/i18n/translation.fr.js
@@ -14,19 +14,20 @@ export default {
   classes: {
     common: 'tous les Canadiens',
     'ca-qc': 'résidents du Québec',
-    'ca-ab': 'résidents de l\'Alberta',
-    'ca-on': 'résidents de l\'Ontario',
+    'ca-ab': "résidents de l'Alberta",
+    'ca-on': "résidents de l'Ontario",
     'ca-ns': 'résidents de la Nouvelle-Écosse',
     'ca-nb': 'résidents du Nouveau-Brunswick',
     'ca-mb': 'résidents du Manitoba',
     'ca-bc': 'résidents de la Colombie-Britannique',
     'ca-sk': 'résidents de la Saskatchewan',
-    'ca-pe': 'résidents de l\'Île-du-Prince-Édouard',
+    'ca-pe': "résidents de l'Île-du-Prince-Édouard",
     'ca-yt': 'résidents du Yukon',
     'ca-nt': 'résidents des Territoires du Nord',
     'ca-nl': 'résidents de Terre-Neuve-et-Labrador',
     'ca-nu': 'Les habitants de Nuvavut',
     'elevated-medical-risk': 'les personnes présentant un risque médical élevé',
-    'elevated-covid-risk': 'les personnes présentant un risque élevé d\'infection'
+    'elevated-covid-risk':
+      "les personnes présentant un risque élevé d'infection"
   }
 }
