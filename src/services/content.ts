3c518944cde57b4315cf0ddef01aeacfa93231d7
Victor Repkow <victor.repkow@dialogue.co>
2020-03-10 13:44:47 -0400

    fix(results): order classes based on query string (#104)

--- src/services/content.ts
+++ src/services/content.ts
@@ -1,10 +1,14 @@
 import i18n from 'services/i18n'
 import contentManifest from 'content/manifest.json'
 
-export const getManifestFilesFromClasses = classes =>
-  contentManifest.filter(file => classes.indexOf(file.class) !== -1)
+export const getManifestFilesFromClasses = (classes: string[]) =>
+  classes
+    .flatMap(className =>
+      contentManifest.filter(file => file.class === className)
+    )
+    .filter(Boolean)
 
-export const getLocalizedFilePathsFromClasses = classes =>
+export const getLocalizedFilePathsFromClasses = (classes: string[]) =>
   getManifestFilesFromClasses(classes)
     .filter(file => i18n.language.indexOf(file.lang) !== -1)
     .map(file => file.path)
