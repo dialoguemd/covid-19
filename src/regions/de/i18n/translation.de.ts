6417be7be7cb72a0ba01e7ab149f27eece1aaa69
Jared Savage <jared.savage@dialogue.co>
2020-03-11 22:37:58 -0400

    chore: update content locations and builder

---
+++ src/regions/de/i18n/translation.de.ts
@@ -0,0 +1,70 @@
+export default {
+  resultsPage: {
+    headerTitle: 'COVID-19: What you need to know',
+    noResultsMessage: 'No results provided in URL.',
+    changeAudienceTitle: 'Doesn’t apply to you?',
+    changeAudience: 'Get personalized info',
+    audiencePrefix: 'Information below applies to:'
+  },
+  share: {
+    viewResults: 'View information package',
+    twitterButton: 'Share on Twitter',
+    facebookButton: 'Share on Facebook',
+    twitterShareText:
+      'Latest COVID-19 info from Canadian Government %23coronavirus {{url}}'
+  },
+  welcomePage: {
+    title: 'COVID-19: What you need to know',
+    description:
+      'Get accurate and personalized information from trusted Canadian medical sources regarding COVID-19.',
+    button: 'Get Started'
+  },
+  classes: {
+    common: 'all Canadians',
+    'ca-qc': 'residents of Quebec',
+    'ca-ab': 'residents of Alberta',
+    'ca-on': 'residents of Ontario',
+    'ca-ns': 'residents of Nova Scotia',
+    'ca-nb': 'residents of New Brunswick',
+    'ca-mb': 'residents of Manitoba',
+    'ca-bc': 'residents of British Columbia',
+    'ca-sk': 'residents of Saskatchewan',
+    'ca-pe': 'residents of Prince Edward Island',
+    'ca-yt': 'residents of Yukon',
+    'ca-nt': 'residents of Northern Territories',
+    'ca-nl': 'residents of Newfoundland and Labrador',
+    'ca-nu': 'residents of Nuvavut',
+    'travel-plans': 'people with travel plans',
+    'elevated-medical-risk': 'people with an increased risk of complications',
+    'elevated-covid-risk': 'people with an increased risk of infection'
+  },
+  footer: {
+    about: 'About',
+    aboutThisSite: 'About this site',
+    githubProject: 'GitHub Project',
+    dialogue: 'Dialogue',
+    organizationResources: 'Resources for Canadian Organizations',
+    contactUs: 'Contact Dialogue',
+    forProvince: 'For Provincial Residents',
+    forCanadians: 'For Canadians',
+    generalInfo: 'General information',
+    elevatedInfection: 'People with an increased risk of infection',
+    elevatedMedical: 'People with an increased risk of complications',
+    travelPlans: 'People with travel plans'
+  },
+  provinces: {
+    QC: 'Quebec',
+    AB: 'Alberta',
+    ON: 'Ontario',
+    NS: 'Nova Scotia',
+    NB: 'New Brunswick',
+    MB: 'Manitoba',
+    BC: 'British Columbia',
+    SK: 'Saskatchewan',
+    PE: 'Prince Edward Island',
+    YT: 'Yukon',
+    NT: 'Northern Territories',
+    NL: 'Newfoundland and Labrador',
+    NU: 'Nuvavut'
+  }
+}
