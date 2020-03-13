333876442223592a0ae927aae9f15c0e15055fad
Jared Savage <jared.savage@dialogue.co>
2020-03-13 15:15:25 -0400

    feat: add footer to markdown

--- src/services/results-loader.ts
+++ src/services/results-loader.ts
@@ -10,15 +10,20 @@ export const getFilePath = async (filePath: string): Promise<string | null> => {
   }
 }
 
+export const injectFooterHtml = (content, id) =>
+  content && content + '\n<ResultsFooter id="' + id + '" />\n'
+
 export const loadResults = async (filePaths: string[]): Promise<string> => {
-  const resultFiles = (await Promise.all(filePaths.map(getFilePath))).filter(
-    Boolean
-  )
+  const resultFiles = await Promise.all(filePaths.map(getFilePath))
 
   const fileContents = await Promise.all(
-    resultFiles.map(url => fetch(url).then(res => res.text()))
+    resultFiles.map(url => url && fetch(url).then(res => res.text()))
   )
 
-  // join file textx, add a MD horizontal rule between them
-  return fileContents.join('\n---\n')
+  const resultsSet = fileContents
+    .map((content, i) => injectFooterHtml(content, filePaths[i]))
+    .filter(Boolean)
+
+  // join file text, add a MD horizontal rule between them
+  return resultsSet.join('\n---\n')
 }
