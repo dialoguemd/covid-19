465712deee0bd99f203b750b35afec10eeab443b
Florian Parga <florian.parga@nuecho.com>
2020-04-09 12:22:13 -0400

    chore: formatting

--- src/regions/de/i18n/translation.de.ts
+++ src/regions/de/i18n/translation.de.ts
@@ -6,7 +6,7 @@ export default {
     changeAudience: 'Chatbot erneut starten',
     audiencePrefix: 'Die folgenden Informationen gelten für:',
     lastModified: 'Aktualisiert {{date}}',
-    feedbackLabel: 'War das nützlich?',
+    feedbackLabel: 'War das nützlich?'
   },
   share: {
     viewResults: 'Informationspaket anzeigen',
@@ -14,7 +14,7 @@ export default {
     facebookButton: 'Auf Facebook teilen',
     twitterShareText:
       'Neueste COVID-19-Informationen der deutschen Regierung %23coronavirus {{url}}',
-    CTA: 'Verbreiten Sie die Information, nicht den Virus',
+    CTA: 'Verbreiten Sie die Information, nicht den Virus'
   },
   welcomePage: {
     title: 'COVID-19: Was Sie wissen müssen',
@@ -23,17 +23,17 @@ export default {
     button: 'START',
     previousRunLink: 'Neueste Informationen für Ihre letzte Sitzung anzeigen',
     previousRunExpired:
-      'Die Antworten aus der letzten Sitzung sind abgelaufen. Bitte starten Sie einen neuen Chat, um aktuelle Informationen zu erhalten.',
+      'Die Antworten aus der letzten Sitzung sind abgelaufen. Bitte starten Sie einen neuen Chat, um aktuelle Informationen zu erhalten.'
   },
   rasaChatWidget: {
-    title: 'COVID-19',
+    title: 'COVID-19'
   },
   classes: {
     common: 'Allgemeine Informationen',
     de: 'Personen jeder deutschen Region',
     'travel-plans': 'Personen mit bevorstehender Reise',
     'elevated-medical-risk': 'Personen die einer Risikogruppe zugehören',
-    'elevated-covid-risk': 'Personen mit einem erhöhten Infektionsrisiko',
+    'elevated-covid-risk': 'Personen mit einem erhöhten Infektionsrisiko'
   },
   footer: {
     bottomText:
@@ -43,7 +43,7 @@ export default {
       ['Über uns', 'https://github.com/dialoguemd/covid-19/wiki'],
       ['GitHub Project', 'https://github.com/dialoguemd/covid-19'],
       ['Dialogue', 'https://www.dialogue.co/en'],
-      ['Kontakt', 'hhttps://www.dialogue.co/en/contact-us'],
+      ['Kontakt', 'hhttps://www.dialogue.co/en/contact-us']
     ],
     adminAreaHeader: 'Regionale Informationen',
     adminAreaMenu: [],
@@ -52,7 +52,7 @@ export default {
       ['Allgemeine Informationen', 'common'],
       ['Personen mit einem erhöhten Infektionsrisiko', 'elevated-covid-risk'],
       ['Risikogruppen', 'elevated-medical-risk'],
-      ['Reisende und Personen mit Reiseabsichten', 'travel-plans'],
-    ],
-  },
+      ['Reisende und Personen mit Reiseabsichten', 'travel-plans']
+    ]
+  }
 }
