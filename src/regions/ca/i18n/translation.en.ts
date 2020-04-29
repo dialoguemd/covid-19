c76e290f1a73c8eb2b69a17c572e61b2afd6398a
Alexis Smirnov <alexis@smirnov.ca>
2020-04-29 14:23:58 -0400

    fix: qa bot flow (#703)
    * streamlined Q&A flow
    
    * remove question collection CTA
    
    * lint
    
    * copy tweaks
    
    * copy tweaks

--- src/regions/ca/i18n/translation.en.ts
+++ src/regions/ca/i18n/translation.en.ts
@@ -64,10 +64,7 @@ export default {
         'https://www.dialogue.co/?hs_preview=noJtvihk-26668052747'
       ],
       ['Contact Dialogue', 'https://www.dialogue.co/en/contact-us/'],
-      [
-        'Help build AI. Ask a COVID-19 question.',
-        'https://covid19.dialogue.co/#/info'
-      ]
+      ['Ask a COVID-19 question.', 'https://covid19.dialogue.co/#/info']
     ],
     adminAreaHeader: 'For Provincial Residents',
     adminAreaMenu: [
