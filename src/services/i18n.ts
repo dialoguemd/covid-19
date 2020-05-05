372f34a37a3da1d13907b557cd9d2c7be3daff67
Florian Parga <florian.parga@nuecho.com>
2020-05-05 13:40:46 -0400

    fix: use caching for i18n (#711)

--- src/services/i18n.ts
+++ src/services/i18n.ts
@@ -1,20 +1,41 @@
 import i18n from 'i18next'
 import i18nLanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
+import queryString from 'query-string'
+import { createBrowserHistory } from 'history'
+
 import { requireRegionFile } from 'services/region-loader'
 import { config } from 'services/config'
 
 const { ENABLED_LANGUAGES } = config
 
-const langToResources = lang => ({
+const LANGUAGE_QUERY_STRING = 'lng'
+
+const history = createBrowserHistory()
+
+const langToResources = (lang: string) => ({
   steps: requireRegionFile(`i18n/steps.${lang}.ts`).default,
   translation: requireRegionFile(`i18n/translation.${lang}.ts`).default
 })
 
 const resources = ENABLED_LANGUAGES.reduce(
-  (acc, lang) => ({ ...acc, [lang]: langToResources(lang) }),
+  (acc: object, lang: string) => ({ ...acc, [lang]: langToResources(lang) }),
   {}
 )
+
+i18n.on('initialized', () => {
+  const location = history.location
+  const parsed = queryString.parse(location.search)
+
+  // remove lng querystring from url if present after initialization
+  if (parsed[LANGUAGE_QUERY_STRING]) {
+    const search = queryString.stringify({
+      ...parsed,
+      [LANGUAGE_QUERY_STRING]: undefined
+    })
+    history.push({ ...history.location, search })
+  }
+})
 
 i18n
   .use(i18nLanguageDetector)
@@ -29,8 +50,9 @@ i18n
     load: 'languageOnly',
     appendNamespaceToMissingKey: true,
     detection: {
-      order: ['querystring', 'navigator', 'htmlTag'],
-      caches: []
+      order: ['querystring', 'localStorage', 'navigator', 'htmlTag'],
+      caches: ['localStorage'],
+      lookupQuerystring: LANGUAGE_QUERY_STRING
     }
   })
 
