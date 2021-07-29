19dbf97389c2985edda0e646d67c05de1d5d4029
Victor Repkow <victor.repkow@dialogue.co>
2021-07-29 15:17:23 -0400

    chore: tests

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import App from './app'
 
+import './services/i18n'
+
 test('runs the test', () => {
   render(<App />)
   expect(true).toBe(true)
